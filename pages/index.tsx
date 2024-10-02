9a7a68fca69b288af25dbeb0f5df39b3611edc41
lebrancconvas <lebrancconvas@gmail.com>
2024-10-02 12:35:35 +0700

    ✨ feat: update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@
  * Written by and Copyright (C) 2021 Shingo OKAWA shingo.okawa.g.h.c@gmail.com
  * Trademarks are owned by their respect owners.
  */
+import Head from 'next/head';
 import styles from '../styles/Home.module.css'
 import {
   NES
@@ -12,6 +13,9 @@ import {
 export default function Home() {
   return (
     <div className={styles.root}>
+      <Head>
+        <title>NES TypeScript</title>
+      </Head>
       <NES width={256} height={240}/>
     </div>
   );
